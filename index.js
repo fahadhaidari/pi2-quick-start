196c06d994ee078ce851a8bf733f3b6e25c4cd96
Fahad Haidari <fahadhaidari@Fahads-MacBook-Pro.local>
2019-07-07 22:32:59 -0600

    Push demo

---
+++ index.js
@@ -0,0 +1,26 @@
+pi2.setup = function () {
+  this.width = 400;
+  this.height = 400;
+  this.color = '#111111';
+  this.friction = 0.999;
+  this.gravity = { x: 0, y: 0 };
+  this.border = {
+    color: '#EEEEEE',
+    width: 10,
+  }
+}
+
+pi2.start();
+
+pi2.onMouseUp = function(target) {
+  pi2.draw.circle({
+    x: target.mouse.x,
+    y: target.mouse.y,
+    vel: new pi2.Vector(
+      pi2.util.randomDouble(-2, 2),
+      pi2.util.randomDouble(-2, 2),
+    ),
+    radius: pi2.util.randomInt(5, 20),
+    color: pi2.util.randomColor(),
+  });
+};
